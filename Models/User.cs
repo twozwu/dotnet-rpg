086ad82e5120bb79fe196747ef8e90c6688d3c14
Mix <honter_tribor@hotmail.com>
2023-05-19 01:53:29 +0800

    User Registration

--- Models/User.cs
+++ Models/User.cs
@@ -4,7 +4,7 @@ namespace dotnet_rpg.Models
     {
         public int Id { get; set; }
         public string Usernames { get; set; } = string.Empty;
-        public byte[] PasswordHash { get; private set; } = new byte[0];
+        public byte[] PasswordHash { get; set; } = new byte[0];
         public byte[] PasswordSalt { get; set; } = new byte[0];
         public List<Character>? Characters { get; set; }
     }
