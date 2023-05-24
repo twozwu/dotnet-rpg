979be1ca5d613beb59814dc0b8775141301239bb
at3254 (Mix) <honter_tribor@hotmail.com>
2023-05-24 16:31:05 +0800

    User Login

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -21,7 +21,18 @@ namespace dotnet_rpg.Controllers
                 // new User用物件是因為可以讓你包含更多信息進去，例如email、生日等等
                 new User { Username = request.Username }, request.Password
             );
-            if(!response.Success)
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+
+        [HttpPost("Login")]
+        public async Task<ActionResult<ServiceResponse<int>>> Login(UserLoginDto request)
+        {
+            var response = await _authRepo.Login(request.Username, request.Password);
+            if (!response.Success)
             {
                 return BadRequest(response);
             }
--- Data/AuthRepository.cs
+++ Data/AuthRepository.cs
@@ -9,9 +9,26 @@ namespace dotnet_rpg.Data
             _context = context;
         }
 
-        public Task<ServiceResponse<string>> Login(string username, string password)
+        public async Task<ServiceResponse<string>> Login(string username, string password)
         {
-            throw new NotImplementedException();
+            var response = new ServiceResponse<string>();
+            var user = await _context.Users
+                .FirstOrDefaultAsync(u => u.Username.ToLower().Equals(username.ToLower()));
+            if (user is null)
+            {
+                response.Success = false;
+                response.Message = "User not found.";
+            }
+            else if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+            {
+                response.Success = false;
+                response.Message = "Wrong password.";
+            }
+            else
+            {
+                response.Data = user.Id.ToString();
+            }
+            return response;
         }
 
         public async Task<ServiceResponse<int>> Register(User user, string password)
@@ -28,7 +45,7 @@ namespace dotnet_rpg.Data
             user.PasswordHash = passwordHash;
             user.PasswordSalt = passwordSalt;
 
-            _context.Users.Add(user);
+            _context.Users.Add(user); // 分成兩個欄位儲存
             await _context.SaveChangesAsync();
             response.Data = user.Id;
             return response;
@@ -47,8 +64,17 @@ namespace dotnet_rpg.Data
         {
             using (var hmac = new System.Security.Cryptography.HMACSHA512())
             {
-                passwordSalt = hmac.Key;
+                passwordSalt = hmac.Key; // 先加鹽再hash
                 passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt)) // 上次產生的加鹽值拿來用
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);
             }
         }
     }
