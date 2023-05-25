38a10d94b2000c5aada71e201bc92b3b7de18797
at3254 (Mix) <honter_tribor@hotmail.com>
2023-05-25 11:56:57 +0800

    JSON Web Tokens (JWT) implementations

--- Data/AuthRepository.cs
+++ Data/AuthRepository.cs
@@ -1,12 +1,18 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+
 namespace dotnet_rpg.Data
 {
     public class AuthRepository : IAuthRepository
     {
         private readonly DataContext _context;
+        private readonly IConfiguration _configuration;
 
-        public AuthRepository(DataContext context)
+        public AuthRepository(DataContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
         }
 
         public async Task<ServiceResponse<string>> Login(string username, string password)
@@ -26,7 +32,7 @@ namespace dotnet_rpg.Data
             }
             else
             {
-                response.Data = user.Id.ToString();
+                response.Data = CreatToken(user);
             }
             return response;
         }
@@ -76,6 +82,37 @@ namespace dotnet_rpg.Data
                 var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
                 return computedHash.SequenceEqual(passwordHash);
             }
+        }
+
+        private string CreatToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()), // 建立姓名識別碼
+                new Claim(ClaimTypes.Name, user.Username) // 建立姓名資料
+            };
+
+            var appSettingsToken = _configuration.GetSection("AppSettings:Token").Value;
+            if (appSettingsToken is null)
+                throw new Exception("AppSettings Token is null!");
+
+            // 建立對稱安全密鑰
+            SymmetricSecurityKey key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(appSettingsToken)); // 將字串(string)轉位元(Byte)
+
+            // 建立簽章 (使用HMC SHA 512演算法)
+            SigningCredentials creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1), // 一天後過期
+                SigningCredentials = creds // 簽章
+            };
+
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler(); // JWT創建模組實體化
+            SecurityToken token = tokenHandler.CreateToken(tokenDescriptor); // 使用JWT模組建立token物件
+
+            return tokenHandler.WriteToken(token); // 回傳序列化後的 Token 結果(字串格式)
         }
     }
 }
