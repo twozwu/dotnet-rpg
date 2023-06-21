f7d4ffb436734e3b1c4ad5dd5627b525a899e581
at3254 (Mix) <honter_tribor@hotmail.com>
2023-06-21 17:11:17 +0800

    Data Seeding - Add Skills to the Database

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -5,6 +5,16 @@ namespace dotnet_rpg.Data
         public DataContext(DbContextOptions<DataContext> options)
             : base(options) { }
 
+        // 當模型創建時會插入資料
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Skill>().HasData(
+                 new Skill { Id = 1, Name = "Fireball", Damage = 30 },
+                 new Skill { Id = 2, Name = "Frenzy", Damage = 20 },
+                 new Skill { Id = 3, Name = "Blizzard", Damage = 50 }
+            );
+        }
+
         // Dbset 為建立 Characters 的資料表
         public DbSet<Character> Characters => Set<Character>();
         public DbSet<User> Users => Set<User>();
